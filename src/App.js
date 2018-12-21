271c262a37d6e54bf23ca44e934ac27e6d0d9b23
joakimolofsson <jjoakim.olofsson@gmail.com>
2018-12-21 11:47:05 +0100

    Added simple react-router function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import './App.css';
+
 import Weather from './components/Weather';
 import SavedWeather from './components/SavedWeather';
+import About from './components/About';
+import Home from './components/Home';
 
 class App extends Component {
   state = {
@@ -18,12 +22,20 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className='Nav'>
-          <Weather saveWeatherData={this.saveWeatherData}/>
-          <SavedWeather weatherData={this.state}/>
+      <Router>
+        <div className="App">
+          <div className='Nav'>
+            <Weather saveWeatherData={this.saveWeatherData}/>
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            <Link to="/save">Save</Link>
+          </div>
+
+          <Route path="/" exact component={Home} />
+          <Route path="/about" component={About} />
+          <Route path="/save" render={() => <SavedWeather weatherData={this.state}/>} />
         </div>
-      </div>
+      </Router>
     )
   }
 }
